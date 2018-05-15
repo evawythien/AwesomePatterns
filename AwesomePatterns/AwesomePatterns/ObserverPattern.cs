bbcc7ab1062ef4b8ab57aec9b36a2eb55460ea57
evawythien <evadvgarcia@gmail.com>
2018-05-15 23:25:55 +0200

    Complete observer, add strategy

--- AwesomePatterns/AwesomePatterns/ObserverPattern.cs
+++ AwesomePatterns/AwesomePatterns/ObserverPattern.cs
@@ -2,37 +2,58 @@
 
 namespace AwesomePatterns
 {
-	class ObserverPattern
-	{
-		static void Main(string[] args)
-		{
-			Book book = new Book();
-			book.Name = "OriginalBookName";
-			book.State = "FAIL";
-		}
-	}
+    class ObserverPattern
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.Name = "OriginalBookName";
+            book.Action = "S";
 
-	// Observador
-	public class Book
-	{
-		public string Name { get; set; }
-		public string State { get; set; }
-	}
+            Sell sell = new Sell();
+            sell.Update(book);
 
-	public class Sell : IBrokenBook
-	{
-		public void Update() => Console.WriteLine("You just sold a book");
-	}
+            Buy buy = new Buy();
+            buy.Update(book);
 
-	public class Buy : IBrokenBook
-	{
-		public void Update() => Console.WriteLine("You just bought a book");
-	}
+            Stock stock = new Stock();
+            stock.Update(book);
+        }
+    }
 
-	public class Stock : IBrokenBook
-	{
-		public void Update() => Console.WriteLine("Checking stock of the book");
-	}
+    // Observador
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Action { get; set; }
+    }
 
-	public interface IBrokenBook { void Update(); }
+    public class Sell : IBrokenBook
+    {
+        public void Update(Book book)
+        {
+            if (book.Action.Equals("S"))
+                Console.WriteLine("You just sold a book");
+        }
+    }
+
+    public class Buy : IBrokenBook
+    {
+        public void Update(Book book)
+        {
+            if (book.Action.Equals("B"))
+                Console.WriteLine("You just bought a book");
+        }
+    }
+
+    public class Stock : IBrokenBook
+    {
+        public void Update(Book book)
+        {
+            if (book.Action.Equals("ST"))
+                Console.WriteLine("Checking stock of the book");
+        }
+    }
+
+    public interface IBrokenBook { void Update(Book book); }
 }
