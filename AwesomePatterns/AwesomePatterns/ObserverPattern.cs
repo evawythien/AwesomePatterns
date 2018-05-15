62f91dff02a60b6753427ab544b10c70dfaeb186
evawythien <evadvgarcia@gmail.com>
2018-05-15 22:45:19 +0200

    Observer pattern

---
+++ AwesomePatterns/AwesomePatterns/ObserverPattern.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AwesomePatterns
+{
+	class ObserverPattern
+	{
+		static void Main(string[] args)
+		{
+			Book book = new Book();
+			book.Name = "OriginalBookName";
+			book.State = "FAIL";
+		}
+	}
+
+	// Observador
+	public class Book
+	{
+		public string Name { get; set; }
+		public string State { get; set; }
+	}
+
+	public class Sell : IBrokenBook
+	{
+		public void Update() => Console.WriteLine("You just sold a book");
+	}
+
+	public class Buy : IBrokenBook
+	{
+		public void Update() => Console.WriteLine("You just bought a book");
+	}
+
+	public class Stock : IBrokenBook
+	{
+		public void Update() => Console.WriteLine("Checking stock of the book");
+	}
+
+	public interface IBrokenBook { void Update(); }
+}
