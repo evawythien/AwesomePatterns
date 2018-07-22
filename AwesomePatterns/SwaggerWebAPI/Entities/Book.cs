9d19a25d23dd226850e99eaf598472212e5ae66f
evawythien <evadvgarcia@gmail.com>
2018-07-22 19:20:14 +0200

    Add API swagger project and test project

---
+++ AwesomePatterns/SwaggerWebAPI/Entities/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SwaggerWebAPI
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Editorial { get; set; }
+        public string SerialNumber { get; set; }
+        public string Stock { get; set; }
+    }
+}
---
+++ AwesomePatterns/SwaggerWebAPITest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace SwaggerWebAPITest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            bool option = true;
+            Assert.True(option);
+        }
+    }
+}
