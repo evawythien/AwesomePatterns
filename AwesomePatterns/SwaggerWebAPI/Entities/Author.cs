f7e2eba5e6ec03da67db0cfc1e88d6de923ec277
evawythien <evadvgarcia@gmail.com>
2018-07-22 19:48:43 +0200

    Identities changes.

---
+++ AwesomePatterns/SwaggerWebAPI/Entities/Author.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwaggerWebAPI.Entities
+{
+    class Author
+    {
+        /// <summary>
+        /// Name of author.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Surname of author.
+        /// </summary>
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// Nationality.
+        /// </summary>
+        public string Nationality { get; set; }
+
+        /// <summary>
+        /// Birthday.
+        /// </summary>
+        public DateTime Birth { get; set; }
+
+        /// <summary>
+        /// All books.
+        /// </summary>
+        public List<Book> Publications { get; set; }
+    }
+}
--- AwesomePatterns/SwaggerWebAPI/Entities/Book.cs
+++ AwesomePatterns/SwaggerWebAPI/Entities/Book.cs
@@ -4,10 +4,34 @@ namespace SwaggerWebAPI
 {
     public class Book
     {
+        /// <summary>
+        /// Title.
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Author.
+        /// </summary>
         public string Author { get; set; }
+
+        /// <summary>
+        /// Editorial.
+        /// </summary>
         public string Editorial { get; set; }
+
+        /// <summary>
+        /// Serial number.
+        /// </summary>
         public string SerialNumber { get; set; }
-        public string Stock { get; set; }
+
+        /// <summary>
+        /// Number of pages.
+        /// </summary>
+        public string NumPages { get; set; }
+
+        /// <summary>
+        /// Is in stock.
+        /// </summary>
+        public bool InStock { get; set; }
     }
 }
